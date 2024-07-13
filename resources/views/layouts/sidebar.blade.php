802053e88655fb0b58f3ff01edb0ae3d3f19706b
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-07-13 22:54:01 +0700

    chore: Add user management menu to sidebar and routes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,5 +8,14 @@
     <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
         <li class="active"><a class="nav-link" href="#"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+        <li class="menu-header">Master Data</li>
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users-cog"></i> <span>User</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="{{route('users')}}">User Management</a></li>
+            <li><a class="nav-link" href="{{route('comingsoon')}}">Role Management</a></li>
+            <li><a class="nav-link" href="{{route('comingsoon')}}">Permission Management</a></li>
+          </ul>
+        </li>
       </ul>
   </aside>
---
+++ resources/views/users.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+
+@section('title', 'User Management')
+
+@push('styles')
+    @livewireStyles
+@endpush
+
+@push('scripts')
+    @livewireScripts
+@endpush
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>User Management</h1>
+    </div>
+
+    <div class="section-body">
+        
+    </div>
+</section>
+@endsection
