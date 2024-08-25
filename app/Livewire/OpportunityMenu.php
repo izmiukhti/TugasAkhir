f7a60471cf1448966848f43d15e9509d5c8a350f
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-08-25 20:59:17 +0700

    chore: Refactor OpportunityMenu component to include home and detail functionality

--- app/Livewire/OpportunityMenu.php
+++ app/Livewire/OpportunityMenu.php
@@ -7,9 +7,20 @@ use Livewire\Component;
 class OpportunityMenu extends Component
 {
     public $isHome = true;
-    
+    public $isDetail = false;
+
     public function render()
     {
         return view('livewire.opportunity-menu');
+    }
+
+    public function home(){
+        $this->isHome = true;
+        $this->isDetail = false;
+    }
+
+    public function detail(){
+        $this->isHome = false;
+        $this->isDetail = true;
     }
 }
--- resources/views/livewire/opportunity-menu.blade.php
+++ resources/views/livewire/opportunity-menu.blade.php
@@ -67,7 +67,7 @@
                                 </div>
                             </div>
                             <p>Created Date : 14 Agustus 2024</p>
-                            <a href="#" class="btn btn-block btn-icon icon-left btn-outline-info"><i class="fas fa-info-circle"></i> Detail</a>
+                            <a href="#" wire:click.prevent="detail()" class="btn btn-block btn-icon icon-left btn-outline-info"><i class="fas fa-info-circle"></i> Detail</a>
                         </div>
                     </div>
                 </div>
@@ -80,6 +80,62 @@
                             
                             </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    @endif
+    @if ($isDetail)
+        <div class="section-header">
+            <h1>Detail Opportunity</h1>
+        </div>
+
+        <div class="section-body">
+            <h2 class="section-title">Opportunity Detail</h2>
+            <p class="section-lead">In this section you can show detail of Opportunity.</p>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <h3>Fullstack Developer - Internship</h3>
+                            <p>Kota Malang, WFH | Created Date : 20 Agustus 2024 <br> Open Date : 20 December 2024 | Close Date : 30 December 2024</p>
+                            <p></p>
+                            <div class="badges">
+                                <span class="badge badge-success">Aktif</span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-3 text-center">
+                                    <div class="alert alert-light">
+                                        <p><strong>Jumlah Click</strong></p>
+                                        <h4>1024</h4>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-center">
+                                    <div class="alert alert-light">
+                                        <p><strong>Jumlah Applicant</strong></p>
+                                        <h4>185</h4>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-center">
+                                    <div class="alert alert-light">
+                                        <p><strong>Jumlah Quota</strong></p>
+                                        <h4>10</h4>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-center">
+                                    <a href="#" class="btn btn-sm btn-block btn-outline-dark icon-left"><i class="fas fa-info-circle"></i> Detail</a>
+                                    <a href="#" class="btn btn-sm btn-block btn-outline-warning icon-left"><i class="far fa-edit"></i> Update</a>
+                                    <a href="#" class="btn btn-sm btn-block btn-outline-danger icon-left"><i class="fas fa-times"></i> Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <a href="#" wire:click.prevent="home()" class="btn btn-icon icon-left btn-primary"><i class="fas fa-arrow-left"></i> Back</a>
                 </div>
             </div>
         </div>
