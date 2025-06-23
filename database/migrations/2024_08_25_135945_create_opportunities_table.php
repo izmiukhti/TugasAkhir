60571c14622d1bfc5c05dd9d86232dcf29639dde
Izmi Ukhti <izmiukhti@gmail.com>
2025-06-23 14:37:35 +0700

    perbaiki tabel

--- database/migrations/2024_08_25_135945_create_opportunities_table.php
+++ database/migrations/2024_08_25_135945_create_opportunities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('opportunities', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->longText('description');
             $table->longText('job_description');
             $table->longText('job_requirements');
